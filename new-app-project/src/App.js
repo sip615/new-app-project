fb1bf463cc98662aeec45a58aacf4a114e35a76f
sip615 <sunilpatel.328@gmail.com>
2021-01-27 23:38:45 -0500

    started on the nav

--- new-app-project/src/App.js
+++ new-app-project/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Nav from "../src/components/Nav";
 
 function App() {
   return (
     <div>
-      <h2>New Blank Project</h2>
+      <Nav/>
     </div>
   );
 }
